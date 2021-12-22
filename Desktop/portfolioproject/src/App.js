48ff36611a3de75d8d54a16a2fbaf1491a5e3f67
hind201 <57994987+hind201@users.noreply.github.com>
2021-12-22 11:37:50 +0100

    nouveauD

--- Desktop/portfolioproject/src/App.js
+++ Desktop/portfolioproject/src/App.js
@@ -2,10 +2,42 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import  NavBar from './components/NavBar';
+import Header from './components/Header'
+import Particles from 'react-particles-js';
 
 function App() {
   return (
+    <>
+    <Particles className='particles-canvas'
+      params={{
+        particles: {
+           number:{
+             value:30,
+             density:{
+              enable:true,
+              value_area:900
+             }
+             
+           
+           
+           
+        },
+        shape: {
+          type: 'circle',
+          stroke:{
+            width:6,
+            color:'#FF69B4'
+          }
+      }
+  }
+    }}
+    
+       
+    
+    />
    <NavBar/>
+   <Header/>
+   </>
   );
 }
 
