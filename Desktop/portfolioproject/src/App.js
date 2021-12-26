db1781943b6fae506c48dd525d6e5afcca9b26dd
hind201 <57994987+hind201@users.noreply.github.com>
2021-12-26 22:20:09 +0100

    services-complete

--- Desktop/portfolioproject/src/App.js
+++ Desktop/portfolioproject/src/App.js
@@ -5,6 +5,7 @@ import  NavBar from './components/NavBar';
 import Header from './components/Header'
 import Particles from 'react-particles-js';
 import AboutMe from './components/AboutMe';
+import Services from './components/Services'
 
 function App() {
   return (
@@ -39,6 +40,7 @@ function App() {
    <NavBar/>
    <Header/>
    <AboutMe/>
+   <Services/>
    </>
   );
 }
